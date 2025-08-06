a29cc6009eefd4080b039d0a18cc22bf525851b2
Sanni Jajoriya <sunnyjajoria710@gmail.com>
2025-08-06 14:13:18 +0530

    s

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -36,7 +36,7 @@ export interface User {
   fullName: string;
   email: string;
   password?: string | null;
-  profilePictureUrl?: string;
+  profilePictureUrl?: string | null;
   role: 'student' | 'admin';
   createdAt?: Date;
 }
