1d5fef6883d973401dae3b061ba31ab3e33085c8
SANNI JAJORIYA <sunnyjajoria710@gmail.com>
2025-08-30 11:40:29 +0530

    Update posts.ts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,12 +1,16 @@
+
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
-import remarkGfm from 'remark-gfm';
+import gfm from 'remark-gfm';
+import slugPlugin from 'remark-slug';
+
 
 const postsDirectory = path.join(process.cwd(), 'src/blog');
 
+// List of existing, non-blog routes. This prevents the [slug] route from trying to render them.
 const RESERVED_PATHS = new Set([
   'admin',
   'dashboard',
@@ -23,17 +27,26 @@ const RESERVED_PATHS = new Set([
 ]);
 
 export function getSortedPostsData() {
+  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '');
+
+    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
+
+    // Combine the data with the id
     return {
       id,
       ...(matterResult.data as { date: string; title: string }),
     };
   });
+  // Sort posts by date
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
@@ -45,10 +58,12 @@ export function getSortedPostsData() {
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
+
   const filteredFileNames = fileNames.filter(fileName => {
     const slug = fileName.replace(/\.md$/, '');
     return !RESERVED_PATHS.has(slug);
   })
+
   return filteredFileNames.map((fileName) => {
     return {
       slug: fileName.replace(/\.md$/, ''),
@@ -60,14 +75,22 @@ export async function getPostData(slug: string) {
   if (RESERVED_PATHS.has(slug)) {
     throw new Error('This is a reserved path, not a blog post.');
   }
+
   const fullPath = path.join(postsDirectory, `${slug}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string, with plugins
   const processedContent = await remark()
-    .use(remarkGfm)
-    .use(html, { sanitize: false })
+    .use(gfm) // for tables
+    .use(slugPlugin) // for heading IDs
+    .use(html)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
   return {
     slug,
     contentHtml,
